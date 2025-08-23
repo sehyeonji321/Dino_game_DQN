18a4de541d770569ee13ef8dcd4be1eef07de562
SeHyeon Ji <sehb0321@gmail.com>
2025-08-24 00:17:15 +0900

    ver.7
    target network 추가
    cummulative reward print

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ import torch.optim as optim
 from utils import load_params, save_params
 from config import FINAL_EPSILON, INITIAL_EPSILON, EXPLORE, SAVE_INTERVAL, MODEL_DIR
 from config import OBSERVATION, GAMMA, BATCH_SIZE, LEARNING_RATE
-
+from model import DinoNet
 
 def train_network(model, game_state, observe=False):
     params = load_params()
@@ -21,6 +21,12 @@ def train_network(model, game_state, observe=False):
     optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
     loss_fn = nn.MSELoss()
 
+    ########### Target Network 추가
+    target_model = DinoNet()
+    target_model.load_state_dict(model.state_dict())  # 초기화: model → target_model
+    target_model.eval()
+    TARGET_UPDATE_INTERVAL = 1000  # 몇 step마다 target 네트워크 갱신할지
+
     do_nothing = np.zeros(3)
     do_nothing[0] = 1
 
@@ -29,13 +35,16 @@ def train_network(model, game_state, observe=False):
     s_t = s_t.reshape(1, s_t.shape[0], s_t.shape[1], s_t.shape[2])
 
     OBSERVE = 999999999 if observe else OBSERVATION
+    
+    episode_reward = 0
+    episode_count = 0
 
     while True:
         loss_sum = 0
         a_t = np.zeros([3])
 
         if random.random() <= epsilon:
-            action_index = np.random.choice([0,1,2], p=[0.45, 0.5, 0.05]) # ver.4: 엎드리는 경우는 의도적으로 낮게 sampling
+            action_index = np.random.choice([0,1,2], p=[0.33, 0.34, 0.33]) # ver.4: 엎드리는 경우는 의도적으로 낮게 sampling
             a_t[action_index] = 1
             action_type = "explore"
         else:
@@ -45,30 +54,33 @@ def train_network(model, game_state, observe=False):
             a_t[action_index] = 1
             action_type = "exploit"
 
-        # 나중에 이거 바꿔보기    
+        ###### 요건 eps 감소정책 실험중입니다!!!!!!!!!!!!
         if t > OBSERVE:
             if t <= 100000:
-                # 0 ~ 100k: 0.4 -> 0.1
-                epsilon = INITIAL_EPSILON - (INITIAL_EPSILON - 0.1) * (t / 100_000)
+                # 0 ~ 100k: INITIAL -> 0.05
+                epsilon = INITIAL_EPSILON - (INITIAL_EPSILON - 0.05) * (t / 100000)
             elif t <= 200000:
-                # 100k ~ 200k: 0.1 -> 0.02
-                epsilon = 0.1 - (0.1 - 0.02) * ((t - 100_000) / 100_000)
+                # 100k ~ 200k: 0.05 -> 0.01
+                epsilon = 0.05 - (0.05 - 0.01) * ((t - 100000) / 100000)
             elif t <= 300000:
-                # 200k ~ 300k: 0.02 -> 0.01
-                epsilon = 0.02 - (0.02 - 0.01) * ((t - 200_000) / 100_000)
+                # 200k ~ 300k: 0.01 -> 0.01
+                epsilon = 0.01 - (0.01 - 0.01) * ((t - 200000) / 100000)
             elif t <= 1000000:
-                # 300k ~ 1M: 0.01 -> 0.005
-                epsilon = 0.01 - (0.01 - FINAL_EPSILON) * ((t - 300_000) / 700_000)
+                # 300k ~ 1M: 0.01 -> FINAL
+                epsilon = 0.01 - (0.01 - FINAL_EPSILON) * ((t - 300000) / 700000)
             else:
                 # 이후는 0.005로 고정
                 epsilon = FINAL_EPSILON
 
+        # step 진행
         x_t1, r_t, terminal = game_state.get_state(a_t)
+        episode_reward += r_t   # 보상 누적
+
         x_t1 = x_t1.reshape(1, x_t1.shape[0], x_t1.shape[1], 1)
         s_t1 = np.append(x_t1, s_t[:, :, :, :3], axis=3)
-
-         # deque 자체가 maxlen 있어서 pop 불필요
         D.append((s_t, action_index, r_t, s_t1, terminal))
+
+
 
         if t > OBSERVE:
             minibatch = random.sample(D, BATCH_SIZE)
@@ -79,7 +91,9 @@ def train_network(model, game_state, observe=False):
                 state_t, action_t, reward_t, state_t1, terminal = minibatch[i]
                 inputs[i:i + 1] = state_t
                 target = model(torch.tensor(state_t).float()).detach().numpy()[0]
-                Q_sa = model(torch.tensor(state_t1).float()).detach().numpy()[0]
+
+                ############# target Q값은 target_model로 계산
+                Q_sa = target_model(torch.tensor(state_t1).float()).detach().numpy()[0]
 
                 if terminal:
                     target[action_t] = reward_t
@@ -101,6 +115,10 @@ def train_network(model, game_state, observe=False):
 
         # 다음 상태 업데이트
         if terminal:
+            episode_count += 1
+            print(f"[Episode {episode_count} finished] total reward: {round(episode_reward,2)}")
+            episode_reward = 0  #  리셋
+
             # crash 했으면 초기 상태 다시 쌓기
             do_nothing = np.zeros(3)
             do_nothing[0] = 1
@@ -113,11 +131,22 @@ def train_network(model, game_state, observe=False):
             
         t += 1
 
+        ######### 일정 step마다 target 네트워크 업데이트
+        if t % TARGET_UPDATE_INTERVAL == 0:
+            target_model.load_state_dict(model.state_dict())
+            print(f"🔄 Target network updated at timestep {t}")
+
         if t % SAVE_INTERVAL == 0:
             game_state._game.pause()
             torch.save(model.state_dict(), os.path.join(MODEL_DIR, f"episode_{t}.pth"))
             torch.save(model.state_dict(), "./latest.pth")
             save_params({"D": D, "time": t, "epsilon": epsilon})
             game_state._game.resume()
-
-        print(f'timestep: {t}, epsilon: {round(epsilon, 3)}, action: {action_index} ({action_type}), reward: {r_t}, loss: {round(loss_sum, 3)}')
+            
+            
+        print(
+            f'timestep: {t}, epsilon: {round(epsilon, 3)}, '
+            f'action: {action_index} ({action_type}), '
+            f'reward: {r_t}, loss: {round(loss_sum, 3)}, '
+            f'episode_reward: {round(episode_reward, 2)}'
+        )
